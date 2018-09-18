56dc252a38a124ede887fb82eabcacc7097213e7
komprogram <ongfa.wakhet@iitg.ernet.in>
2018-09-18 13:20:08 +0530

    Add files via upload

---
+++ leavepage.java
@@ -0,0 +1,13 @@
+package in.ac.iitg.iitgphysio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class leavepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leavepage);
+    }
+}
